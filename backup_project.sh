fb869dcfc89a5bcf7b7ddf16b615a6b06522d45c
Anshul <anshul.patil@sada.com>
2026-07-22 15:20:38 +0530

    anshul

--- backup_project.sh
+++ backup_project.sh
@@ -52,8 +52,10 @@ else
     ARCHIVE_PATH="$BACKUP_DEST/$ARCHIVE_NAME"
 
     if [ -f "$ARCHIVE_PATH" ]; then
-        TIMESTAMP=$(date +%s)
-        ARCHIVE_NAME="${PROJECT_NAME}backup${TIMESTAMP}.tar.gz"
+        TIMESTAMP=$(date "+%Y-%m-%d")
+        TIME=$(date "+%H-%M-%S")
+    
+        ARCHIVE_NAME="${PROJECT_NAME}-${TIMESTAMP}-${TIME}.tar.gz"
         ARCHIVE_PATH="$BACKUP_DEST/$ARCHIVE_NAME"
     fi
 
